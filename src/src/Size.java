42078f8ca09b7cf38b137d46dea9c494e991bc93
Lucas Freixieiro <luke.freixieiro@gmail.com>
2021-03-04 13:31:32 +0000

    Documentação

--- src/src/Size.java
+++ src/src/Size.java
@@ -1,3 +1,6 @@
+/**
+ * Enumerado Size qu contêm os tamanhos e as suas características
+ */
 public enum Size {
 
     SMALL(32, 36, "Pequeno",'S'),
@@ -8,25 +11,57 @@ public enum Size {
     private final int maxValue;
     private final char code;
 
+    /**
+     * Construtor do Size
+     * @param minValue Valor minímo
+     * @param maxValue Valor máximo
+     * @param description Descrição do tamanho
+     * @param code Código do tamanho
+     */
     private Size(int minValue, int maxValue, String description, char code){
         this.description=description;
         this.minValue=minValue;
         this.maxValue=maxValue;
         this.code=code;
     }
+
+    /**
+     * Obtem a descrição de forma padrão
+     * @return Descrição
+     */
     @Override
     public String toString() {
         return this.description;
     }
+
+    /**
+     * Obter a descrição do tamanho
+     * @return Descrição do tamanho
+     */
     public String getDescription() {
         return description;
     }
+
+    /**
+     * Obtem o valor minimo do tamanho
+     * @return Valor Minimo
+     */
     public int getMinValue() {
         return minValue;
     }
+
+    /**
+     * Obtem o valor máximo do tamanho
+     * @return Valor Máximo
+     */
     public int getMaxValue() {
         return maxValue;
     }
+
+    /**
+     * Obter o código do tamanho
+     * @return Código do tamanho
+     */
     public char getCode() {
         return code;
     }
